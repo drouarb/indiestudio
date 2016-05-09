401dd8041c57e75eb328e3e79975341267ff7074
trouve_b <defcon2009@laposte.net>
2016-05-09 13:50:55 +0200

    dqspok

---
+++ include/gameData/Item.hh
@@ -0,0 +1,25 @@
+//
+// Item.hh for indie in /home/trouve_b/Desktop/CPP_project/cpp_indie_studio
+// 
+// Made by Alexis Trouve
+// Login   <trouve_b@epitech.net>
+// 
+// Started on  Mon May  9 11:24:05 2016 Alexis Trouve
+// Last update Mon May  9 13:13:48 2016 Alexis Trouve
+//
+
+#ifndef ITEM_HH_
+# define ITEM_HH_
+
+#include "Stats.hh"
+
+class	Item
+{
+private:
+  Stats		mystat;
+public:
+  Item();
+  ~Item();
+};
+
+#endif
