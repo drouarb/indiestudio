ed28da648302a4d96a017a63e01efae994ae69a8
trouve_b <defcon2009@laposte.net>
2016-05-10 19:37:47 +0200

    spell factorry

--- include/gameData/Spell/SpellFactory.hh
+++ include/gameData/Spell/SpellFactory.hh
@@ -5,12 +5,13 @@
 // Login   <trouve_b@epitech.net>
 // 
 // Started on  Mon May  9 18:02:31 2016 Alexis Trouve
-// Last update Tue May 10 17:36:14 2016 Alexis Trouve
+// Last update Tue May 10 19:23:00 2016 Alexis Trouve
 //
 
 #ifndef SPELLFACTORY_HH_
 # define SPELLFACTORY_HH_
 
+#include <vector>
 #include "ASpell.hh"
 #include "World.hh"
 #include "DistAttack.hh"
@@ -25,15 +26,35 @@ namespace gauntlet
   enum		SpellEnum
     {
       MELEE_BARBARE,
-      MELEE_VALKYRIE
+      MELEE_VALKYRIE,
+      DIST_BASIC_CAST_MAGE,
+      DIST_BASIC_ARROW_ELF
     };
 
+  class SpellFactory;
+
+  struct	giveSpellStruct
+  {
+    SpellEnum		spellEnum;
+    gauntlet::ASpell	*(gauntlet::SpellFactory::*givePtr)();
+  };
+  
   class	SpellFactory
   {
+  private:
+    std::vector<giveSpellStruct>	spellTab;
+  private:
+    int			giveNextId();
+    void		fillSpellTab();
+
+    gauntlet::ASpell	*giveMeleeBarbare();
+    gauntlet::ASpell	*giveMeleeValkyrie();
+    gauntlet::ASpell	*giveDistBasicCastMage();
+    gauntlet::ASpell	*giveDistBasicArrowElf();
   public:
     SpellFactory();
     ~SpellFactory();
-    gauntlet::ASpell	*giveSpell(gauntlet::SpellEnum type);
+    gauntlet::ASpell	*giveSpell(gauntlet::SpellEnum typeSpell);
   };
 };
 
--- src/gameData/Spell/SpellFactory.cpp
+++ src/gameData/Spell/SpellFactory.cpp
@@ -5,7 +5,7 @@
 // Login   <trouve_b@epitech.net>
 // 
 // Started on  Mon May  9 18:00:14 2016 Alexis Trouve
-// Last update Tue May 10 16:47:39 2016 Alexis Trouve
+// Last update Tue May 10 19:33:30 2016 Alexis Trouve
 //
 
 #include "SpellFactory.hh"
@@ -14,7 +14,7 @@ using namespace gauntlet;
 
 SpellFactory::SpellFactory()
 {
-
+  fillSpellTab();
 }
 
 SpellFactory::~SpellFactory()
@@ -22,7 +22,65 @@ SpellFactory::~SpellFactory()
 
 }
 
+int		SpellFactory::giveNextId()
+{
+  static	int i = -1;
+
+  ++i;
+  return (i);
+}
+
+void		SpellFactory::fillSpellTab()
+{
+  spellTab.push_back({MELEE_BARBARE, &SpellFactory::giveMeleeBarbare});
+  spellTab.push_back({MELEE_VALKYRIE, &SpellFactory::giveMeleeValkyrie});
+  spellTab.push_back({DIST_BASIC_CAST_MAGE, &SpellFactory::giveDistBasicCastMage});
+  spellTab.push_back({DIST_BASIC_ARROW_ELF, &SpellFactory::giveDistBasicArrowElf});
+}
+
+ASpell		*SpellFactory::giveMeleeBarbare()
+{
+  ASpell	*my;
+
+  my = new MeleeAttack(false, 10, 50, 10.0, 3.0, giveNextId(), "Melee attack of the barbare //esteban", 0.3);
+  return (my);
+}
+
+ASpell		*SpellFactory::giveMeleeValkyrie()
+{
+  ASpell	*my;
+
+  my = new MeleeAttack(false, 15, 5, 18.0, 0.0, giveNextId(), "Melee attack of the valkyrie //esteban", 0.5);
+  return (my);
+}
+
+ASpell		*SpellFactory::giveDistBasicCastMage()
+{
+  ASpell	*my;
+
+  my = new DistAttack(false, 25, 0, 1, 120, 15.0, giveNextId(), "distance basic of the mage //esteban", 0.9);
+  return (my);
+}
+
+ASpell		*SpellFactory::giveDistBasicArrowElf()
+{
+  ASpell	*my;
+
+  my = new DistAttack(false, 10, 0, 1, 250.0, 1.0, giveNextId(), "distance basic attack of the elf //esteban", 0.3);
+  return (my);
+}
+
+
 ASpell		*SpellFactory::giveSpell(SpellEnum type)
 {
+  unsigned int	i;
+
+  i = 0;
+  while (i < spellTab.size())
+    {
+      if (type == spellTab[i].spellEnum)
+	return ((this->*spellTab[i].givePtr)());
+      ++i;
+    }
   return (NULL);
 }
