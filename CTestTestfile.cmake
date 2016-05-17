42cbd8942083bc941c02d5001e2e2290c026059e
Célérien Yohann <yohann.celerien@epitech.eu>
2016-05-17 14:01:25 +0200

    enumPosition

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/celeri_y/cpp_indie_studio
+# Build directory: /home/celeri_y/cpp_indie_studio
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
