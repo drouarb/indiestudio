75e964ebd1e33d750687e8280b80f511fd260fdf
trouve_b <defcon2009@laposte.net>
2016-05-18 11:06:06 +0200

    koala

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/celeri_y/cpp_indie_studio
-# Build directory: /home/celeri_y/cpp_indie_studio
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
