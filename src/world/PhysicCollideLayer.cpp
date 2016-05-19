69575f3f8691c6e7e63af9d77b558a8c634a6789
guemas_g <gregoire.guemas@epitech.eu>
2016-05-19 15:46:26 +0200

    Physic COllider WiP

--- src/world/PhysicCollideLayer.cpp
+++ src/world/PhysicCollideLayer.cpp
@@ -8,49 +8,93 @@
 // Last update Wed May 18 17:19:07 2016 Alexis Trouve
 //
 
-#include "PhysicCollideLayer.hh"
+#include <iostream>
+#include "world/PhysicCollideLayer.hh"
 
 using namespace gauntlet;
 using namespace world;
 
-PhysicCollideLayer::PhysicCollideLayer(double sizex, double sizey) {
-    this->sizeX = sizex;
-    this->sizeY = sizey;
-    int d = sizex * sizey / BIT_IN_BYTE;
-    this->_data = new char[d];
+PhysicCollideLayer::PhysicCollideLayer(double sizex, double sizey) : sizeX(sizex), sizeY(sizey) {
+    this->_data = new char[static_cast<int>(sizex * sizey)];
 }
 
 PhysicCollideLayer::~PhysicCollideLayer() {
 
 }
 
-std::pair<double, double>    PhysicCollideLayer::getSize() {
+std::pair<double, double>    PhysicCollideLayer::getSize() const {
     return (std::make_pair(this->sizeX, this->sizeY));
 }
 
-void PhysicCollideLayer::setWall(unsigned int x, unsigned int y) {
-    unsigned int i = (x * y) / BIT_IN_BYTE;
-    this->_data[i] ^= (-x ^ this->_data[i]) & (1 << ((x * y) % BIT_IN_BYTE));
+void PhysicCollideLayer::setWall(double x, double y) {
+    if (x > this->sizeX || y > this->sizeY || x < 0 || y < 0) {
+        throw std::out_of_range("Out of range");
+    }
+    std::cout << "setWall at x: " << x << " and y: " << y << " <=> _data[" <<
+    static_cast<int>(y) * static_cast<int>(this->sizeX) + static_cast<int>(x) << "]" << std::endl;
+    this->_data[static_cast<int>(y) * static_cast<int>(this->sizeX) + static_cast<int>(x)] = 1;
 }
 
-void PhysicCollideLayer::unsetWall(unsigned int x, unsigned int y) {
+void    PhysicCollideLayer::setWall(double xpos, double ypos, double xsize, double ysize) {
+    for (size_t i = (static_cast<int>(xsize) % 2), pos_x = xpos - xpos / 2 + 1; i < static_cast<int>(xsize) + 1; ++i) {
+        for (size_t j = (static_cast<int>(ysize) % 2), pos_y = ypos - ypos / 2 + 1; j < static_cast<int>(ysize) + 1; ++j) {
+            this->setWall(pos_x + i, pos_y + j);
+        }
+    }
+}
+
+void PhysicCollideLayer::unsetWall(double x, double y) {
+    if (x > this->sizeX || y > this->sizeY || x < 0 || y < 0) {
+        throw std::out_of_range("Out of range");
+    }
+    this->_data[static_cast<int>(y) * static_cast<int>(this->sizeX) + static_cast<int>(x)] = 0;
 }
 
 bool PhysicCollideLayer::isWall(unsigned int x, unsigned int y) {
-    return ((this->_data[x * y / BIT_IN_BYTE] >> ((x * y) % BIT_IN_BYTE)) & 1) == 1;
+    if (x > this->sizeX || y > this->sizeY || x < 0 || y < 0) {
+        throw std::out_of_range("Out of range");
+    }
+    return (this->_data[static_cast<int>(y * this->sizeX + x)] == 1);
 }
 
-void	PhysicCollideLayer::setWallWithSize(double xpos, double ypos, double xsize, double ysize)
-{
-
+void    PhysicCollideLayer::unsetWall(double xpos, double ypos, double xsize, double ysize) {
+    for (size_t i = 0, pos_x = xpos - xpos / 2 + 1; i < static_cast<int>(xsize) + 1; ++i) {
+        for (size_t j = 0, pos_y = ypos - ypos / 2 + 1; j < static_cast<int>(ysize) + 1; ++j) {
+            this->unsetWall(pos_x + i, pos_y + j);
+        }
+    }
 }
 
-void	PhysicCollideLayer::unsetWallWithSize(double xpos, double ypos, double xsize, double ysize)
-{
-
+bool    PhysicCollideLayer::checkCoordSize(double xpos, double ypos, double xsize, double ysize) {
+    for (size_t i = 0, pos_x = xpos - xpos / 2 + 1; i < static_cast<int>(xsize) + 1; ++i) {
+        for (size_t j = 0, pos_y = ypos - ypos / 2 + 1; j < static_cast<int>(ysize) + 1; ++j) {
+            if (this->isWall(pos_x + i, pos_y + j)) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
-bool	PhysicCollideLayer::checkCoordSize(double xpos, double ypos, double xsize, double ysize)
-{
+std::ostream &operator<<(std::ostream &stream, const PhysicCollideLayer &p) {
+    const std::pair<double, double> &pair = p.getSize();
+    double sizeX = pair.first;
+    double sizeY = pair.second;
+    const char *_data = p.getData();
 
+    size_t d = static_cast<size_t>(sizeX * sizeY);
+    for (size_t i = 0; i < d; ++i) {
+        if (i != 0 && (i) % static_cast<int>(sizeX) == 0) {
+            stream << std::endl;
+        }
+        stream << (_data[i] == 0 ? "0" : "1");
+
+    }
+    stream << std::endl;
+    return stream;
+}
+
+
+const char *PhysicCollideLayer::getData() const {
+    return _data;
 }
