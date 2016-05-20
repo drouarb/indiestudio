d748a084b640e58c9cd2374dc7eca357f8cc15a5
celeri_y <yohann.celerien@epitech.eu>
2016-05-20 16:35:32 +0200

    Ogrui hh refractor

--- include/OgreUI.hh
+++ include/OgreUI.hh
@@ -22,39 +22,52 @@
 
 class  OgreUI : public Ogre::FrameListener, public Ogre::WindowEventListener, public OIS::KeyListener, public OIS::MouseListener, OgreBites::SdkTrayListener
 {
-public:
-    OgreUI(void);
-    virtual ~OgreUI(void);
+private:
     std::map<OIS::KeyCode , gauntlet::core::IUIObserver::Key> keymap;
     std::map<OIS::MouseButtonID , gauntlet::core::IUIObserver::Key> mousemap;
 	std::map<gauntlet::core::Position, OgreBites::TrayLocation> posmap;
-    virtual void go(void);
     gauntlet::core::IUIObserver *obs;
-protected:
-    virtual bool setup();
-    virtual void initMap();
-    virtual bool configure(void);
-    virtual void chooseSceneManager(void);
-    virtual void createCamera(void);
-    virtual void createFrameListener(void);
-    virtual void createScene(void);
-    virtual void destroyScene(void);
-    virtual void createViewports(void);
-    virtual void setupResources(void);
-    virtual void createResourceListener(void);
-    virtual void loadResources(void);
-    virtual void buttonHit(OgreBites::Button* button);
-    virtual void itemSelected(OgreBites::SelectMenu* menu);
-    virtual void checkBoxToggled(OgreBites::CheckBox *checkBox);
+	Ogre::Root *mRoot;
+	Ogre::Camera* mCamera;
+	Ogre::SceneManager* mSceneMgr;
+	Ogre::RenderWindow* mWindow;
+	Ogre::String mResourcesCfg;
+	Ogre::String mPluginsCfg;
+	Ogre::OverlaySystem *mOverlaySystem;
+	OgreBites::SdkTrayManager* mTrayMgr;
+	OgreBites::SdkCameraMan* mCameraMan;
+	OgreBites::ParamsPanel* mDetailsPanel;
+	bool mCursorWasVisible;
+	bool mShutDown;
+	OIS::InputManager* mInputManager;
+	OIS::Mouse*    mMouse;
+	OIS::Keyboard* mKeyboard;
+public:
+	void go(void);
+    bool setup();
+    void initMap();
+    bool configure(void);
+    void chooseSceneManager(void);
+    void createCamera(void);
+    void createFrameListener(void);
+    void createScene(void);
+    void destroyScene(void);
+    void createViewports(void);
+    void setupResources(void);
+    void createResourceListener(void);
+    void loadResources(void);
+    void buttonHit(OgreBites::Button* button);
+    void itemSelected(OgreBites::SelectMenu* menu);
+    void checkBoxToggled(OgreBites::CheckBox *checkBox);
     void updateItemValue(int itemid, struct gauntlet::core::t_hitItem item);
-    virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt);
-    virtual bool keyPressed( const OIS::KeyEvent &arg );
-    virtual bool keyReleased( const OIS::KeyEvent &arg );
-     virtual bool mouseMoved( const OIS::MouseEvent &arg );
-    virtual bool mousePressed( const OIS::MouseEvent &arg, OIS::MouseButtonID id );
-    virtual bool mouseReleased( const OIS::MouseEvent &arg, OIS::MouseButtonID id );
-    virtual void windowResized(Ogre::RenderWindow* rw);
-    virtual void windowClosed(Ogre::RenderWindow* rw);
+    bool frameRenderingQueued(const Ogre::FrameEvent& evt);
+    bool keyPressed( const OIS::KeyEvent &arg );
+    bool keyReleased( const OIS::KeyEvent &arg );
+     bool mouseMoved( const OIS::MouseEvent &arg );
+    bool mousePressed( const OIS::MouseEvent &arg, OIS::MouseButtonID id );
+    bool mouseReleased( const OIS::MouseEvent &arg, OIS::MouseButtonID id );
+    void windowResized(Ogre::RenderWindow* rw);
+    void windowClosed(Ogre::RenderWindow* rw);
 	void addButton(gauntlet::core::Position  pos, int id, std::string text, int texture_id);
 	void addSlideBar(gauntlet::core::Position  pos, int id, std::string text, int max_value, int texture_id);
     void addCheckbox(gauntlet::core::Position  pos, int id, std::string text, int texture_id);
@@ -63,26 +76,11 @@ protected:
                                int texture_id);
     void addProgressBar(gauntlet::core::Position pos, int id, std::string text, int texture_id, int value);
     void remove(int ID);
-    virtual void sliderMoved(OgreBites::Slider* slider);
-    virtual void setIObserver(gauntlet::core::IUIObserver * Obs);
+    void sliderMoved(OgreBites::Slider* slider);
+    void setIObserver(gauntlet::core::IUIObserver * Obs);
     void loadSound(std::string & path);
      void hideItem(int id);
     void playSound(int id);
-    Ogre::Root *mRoot;
-    Ogre::Camera* mCamera;
-    Ogre::SceneManager* mSceneMgr;
-    Ogre::RenderWindow* mWindow;
-    Ogre::String mResourcesCfg;
-    Ogre::String mPluginsCfg;
-	Ogre::OverlaySystem *mOverlaySystem;
-    OgreBites::SdkTrayManager* mTrayMgr;
-    OgreBites::SdkCameraMan* mCameraMan;
-    OgreBites::ParamsPanel* mDetailsPanel;
-    bool mCursorWasVisible;
-    bool mShutDown;
-    OIS::InputManager* mInputManager;
-    OIS::Mouse*    mMouse;
-    OIS::Keyboard* mKeyboard;
 };
 
 #endif
