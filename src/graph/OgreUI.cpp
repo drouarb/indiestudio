3ea5e35ee6de2e36f7e451e174561e8201168b03
guemas_g <gregoire.guemas@epitech.eu>
2016-05-23 22:31:06 +0200

    #Warning

--- src/graph/OgreUI.cpp
+++ src/graph/OgreUI.cpp
@@ -615,7 +615,29 @@ bool OgreUI::addWorldEntity(int entityId, EntityName meshid, int x, int y,
     {
       this->moveEntity(entityId, x, y, angle);
       return (true);
+      e = mSceneMgr->createEntity(ss.str(), name);
     }
+  catch (Ogre::Exception & e)
+    {
+      return false;
+    }
+   Ogre::SceneNode *s = worldNode->createChildSceneNode("PLayerNode");
+  this->rootNode = s;
+  s->setPosition(x, y, 0);
+  s->yaw(Ogre::Radian(world::Math::toRad(degres)));
+  rootNode->attachObject(e);
+  rootNode->attachObject(mCamera);
+  mCamera->pitch(Ogre::Degree(-89));
+  mCamera->yaw(Ogre::Degree(20));
+  return (true);
+}
+
+__attribute_deprecated__ __attribute_warn_unused_result__ bool OgreUI::addWorldEntity(int entityId, const std::string &name, int x, int y,
+			    short degres, int texture_id)
+{
+  std::stringstream ss;
+  ss << entityId;
+  Ogre::Entity *e;
   try
     {
       e = mSceneMgr->createEntity(ss.str(), meshmap.at(meshid).c_str());
@@ -632,6 +654,58 @@ bool OgreUI::addWorldEntity(int entityId, EntityName meshid, int x, int y,
   s->attachObject(e);
   return (true);
 }
+
+bool __attribute_warn_unused_result__ OgreUI::addWorldEntity(int entityId,
+							     const std::string &name,
+							     std::pair<int, int> position,
+							     Ogre::Vector3 orientation,
+							     int textureId)
+{
+  std::stringstream ss;
+  ss << entityId;
+  Ogre::Entity *e;
+  try
+    {
+      e = mSceneMgr->createEntity(ss.str(), name);
+    }
+  catch (Ogre::Exception & e)
+    {
+      return false;
+    }
+  Ogre::SceneNode *s = worldNode->createChildSceneNode(ss.str());
+  s->setPosition(position.first, position.second, 0);
+  s->pitch(Ogre::Radian(world::Math::toRad(static_cast<short >(orientation.x))));
+  s->yaw(Ogre::Radian(world::Math::toRad(static_cast<short >(orientation.y))));
+  s->roll(Ogre::Radian(world::Math::toRad(static_cast<short >(orientation.z))));
+  s->attachObject(e);
+  return (true);
+}
+
+#warning "C'est la faute à trouvé"
+
+bool __attribute_warn_unused_result__ OgreUI::addWorldEntity(int entityId,
+							     const std::string &name,
+							     std::pair<int, int> position)
+{
+  return this->addWorldEntity(entityId, name, position, Ogre::Vector3(0, 0, 0), -1);
+}
+
+bool __attribute_warn_unused_result__ OgreUI::addWorldEntity(int entityId,
+							     const std::string &name,
+							     std::pair<int, int> position,
+							     int textureId)
+{
+  return this->addWorldEntity(entityId, name, position, Ogre::Vector3(0, 0, 0), textureId);
+}
+
+bool __attribute_warn_unused_result__ OgreUI::addWorldEntity(int entityId,
+							     const std::string &name,
+							     std::pair<int, int> position,
+							     Ogre::Vector3 orientation)
+{
+  return this->addWorldEntity(entityId, name, position, orientation, -1);
+}
+
 
 
 void OgreUI::setQuality(int percent)
