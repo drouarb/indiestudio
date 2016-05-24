68b674ae982f0373b039a299c588aa9d4db057dd
DROUARD Benjamin <drouar_b@epitech.eu>
2016-05-25 00:09:54 +0200

    Copying PacketHandshake

--- src/core/listeners/ListenerHandshake.cpp
+++ src/core/listeners/ListenerHandshake.cpp
@@ -1,14 +1,13 @@
 #include "ListenerHandshake.hh"
 #include "WaitPacket.hh"
 
-gauntlet::core::ListenerHandshake::ListenerHandshake(Core & core) : core(core)
-{ }
+gauntlet::core::ListenerHandshake::ListenerHandshake(Core &core) : core(core) { }
 
-gauntlet::core::ListenerHandshake::~ListenerHandshake()
-{ }
+gauntlet::core::ListenerHandshake::~ListenerHandshake() { }
 
 void
-gauntlet::core::ListenerHandshake::notify(const network::PacketHandshake * packet)
-{
-  WaitPacket::receive(packet);
+gauntlet::core::ListenerHandshake::notify(const network::PacketHandshake *packet) {
+    WaitPacket::receive(new network::PacketHandshake(packet->getWarrior(), packet->getWizard(), packet->getValkyrie(),
+                                                     packet->getElf(), packet->getMaxPlayers(),
+                                                     packet->getConnectedPlayers()));
 }
