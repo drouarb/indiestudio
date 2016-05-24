c9e59099577cb08efd120818a6a619f8e0b886f3
guemas_g <gregoire.guemas@epitech.eu>
2016-05-24 12:57:33 +0200

    s

--- src/OgreUI.cpp
+++ src/OgreUI.cpp
@@ -612,6 +612,8 @@ void OgreUI::playAnimation(int animationId, int entityId, bool loop)
 			pState->getAnimationName()] = pState;
 }
 
+
+
 std::pair<int, int> OgreUI::getSizeWindow()
 {
   return (std::pair<int, int>(mWindow->getWidth(), mWindow->getHeight()));
