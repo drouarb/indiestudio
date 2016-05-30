db8262503cf7fb08657ceab29bac58de5c5a9825
celeri_y <yohann.celerien@epitech.eu>
2016-05-30 23:04:01 +0200

    camera pos

---
+++ include/gameData/BackgroundName.hh
@@ -0,0 +1,20 @@
+//
+// Created by celeri on 5/30/16.
+//
+
+#ifndef GAUNTLET_BACKGROUNDNAME_HH
+#define GAUNTLET_BACKGROUNDNAME_HH
+
+namespace  gauntlet
+{
+  enum  BackgroundName : int
+  {
+    BACKGROUND_DOOR = 0,
+    BACKGROUND_PLAYERS,
+    BACKGROUND_ROOM,
+    BACKGROUND_CAVE,
+    BACKGROUND_HELL,
+    BACKGROUND_BASIC,
+  };
+};
+#endif //GAUNTLET_BACKGROUNDNAME_HH
