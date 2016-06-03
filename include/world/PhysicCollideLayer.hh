778f961f80cf9ed6573269cd201262689fd3c777
Esteban Lewis <esteban.lewis@epitech.eu>
2016-06-03 22:51:22 +0200

    physic collide layer correction

--- include/world/PhysicCollideLayer.hh
+++ include/world/PhysicCollideLayer.hh
@@ -16,41 +16,38 @@
 # define WALL_MIN_HEIGHT 40
 # define ABS(x) ((x) < 0 ? -(x) : (x))
 
-namespace gauntlet {
-    namespace world {
-        class PhysicCollideLayer {
+namespace gauntlet
+{
+    namespace world
+    {
+        class PhysicCollideLayer
+        {
         private:
-	  double			sizeX;
-	  double			sizeY;
-	  unsigned char			*layer;
-	  HeightMap			heightmap;
-	  std::pair<double, double>	inc;
-	  int				nb_points;
-	  std::pair<double, double>	lastPoint;
-	private:
-	  bool				doOnLine(std::pair<double, double> start, std::pair <double, double> end,
-						 bool (PhysicCollideLayer::*func)(std::pair<double, double>));
-	  bool				checkLine(std::pair<double, double> point);
-	  bool				checkPoint(std::pair<double, double> point);
+            double sizeX;
+            double sizeY;
+            unsigned char *layer;
+            HeightMap heightmap;
+            std::pair<double, double> inc;
+            int nb_points;
+            std::pair<double, double> lastPoint;
+
+        private:
+            bool doOnLine(std::pair<double, double> start, std::pair<double, double> end,
+                          bool (PhysicCollideLayer::*func)(std::pair<double, double>));
+            bool checkLine(std::pair<double, double> point);
+            bool checkPoint(std::pair<double, double> point);
+
         public:
-	  PhysicCollideLayer(const std::string& filePath);
+            PhysicCollideLayer(const std::string &filePath);
+            ~PhysicCollideLayer();
 
-	  ~PhysicCollideLayer();
-
-	  std::pair<double, double>	getSize()const;
-
-	  bool				isWall(const std::pair<double, double>& oldPos,
-					       const std::pair<double, double>& wantedPos);
-
-	  bool				checkCoordSizeCanPass(const std::pair<double, double>& oldPos,
-							      const std::pair<double, double>& wantedPos,
-							      const std::pair<double, double>& size);
-	  bool				checkDiagonal(std::pair<double, double> start,
-						      std::pair<double, double> end,
-						      std::pair<double, double> oldPos,
-						      std::pair<double, double> wantedPos);
-
-	  const unsigned char		*getLayer() const;
+            std::pair<double, double> getSize() const;
+            bool isWall(const std::pair<double, double> &oldPos,
+                        const std::pair<double, double> &wantedPos);
+            bool checkCoordSizeCanPass(const std::pair<double, double> &oldPos,
+                                       const std::pair<double, double> &wantedPos,
+                                       const std::pair<double, double> &size);
+            const unsigned char *getLayer() const;
         };
     };
 };
--- src/world/PhysicCollideLayer.cpp
+++ src/world/PhysicCollideLayer.cpp
@@ -10,205 +10,199 @@
 
 #include <iostream>
 #include <stdexcept>
+#include <math.h>
 #include "world/PhysicCollideLayer.hh"
 
 using namespace gauntlet;
 using namespace world;
 
-PhysicCollideLayer::PhysicCollideLayer(const std::string& filePath)
+PhysicCollideLayer::PhysicCollideLayer(const std::string &filePath)
 {
-  if (!heightmap.load(filePath))
-    {
-      throw (std::runtime_error("Wrong height map"));
-    }
-  sizeX = heightmap.getSize().first;
-  sizeY = heightmap.getSize().second;
+    if (!heightmap.load(filePath))
+        {
+            throw (std::runtime_error("Wrong height map"));
+        }
+    sizeX = heightmap.getSize().first;
+    sizeY = heightmap.getSize().second;
 }
 
 PhysicCollideLayer::~PhysicCollideLayer()
-{}
+{ }
 
 std::pair<double, double>    PhysicCollideLayer::getSize() const
 {
-  return (std::make_pair(sizeX, sizeY));
+    return (std::make_pair(sizeX, sizeY));
 }
 
-bool PhysicCollideLayer::isWall(const std::pair<double, double>& oldPos,
-				const std::pair<double, double>& wantedPos)
+bool PhysicCollideLayer::isWall(const std::pair<double, double> &oldPos,
+                                const std::pair<double, double> &wantedPos)
 {
-  return (ABS(heightmap.at(heightmap.getSize().first - 1 - oldPos.first,
-			   heightmap.getSize().second - 1 - oldPos.second) -
-	      heightmap.at(heightmap.getSize().first - 1 - wantedPos.first,
-			   heightmap.getSize().second - 1 - wantedPos.second))
-	  >= WALL_MIN_HEIGHT);
+    return (ABS(heightmap.at(heightmap.getSize().first - 1 - oldPos.first,
+                             heightmap.getSize().second - 1 - oldPos.second) -
+                heightmap.at(heightmap.getSize().first - 1 - wantedPos.first,
+                             heightmap.getSize().second - 1 - wantedPos.second))
+            >= WALL_MIN_HEIGHT);
 }
 
-bool    PhysicCollideLayer::checkCoordSizeCanPass(const std::pair<double, double>& oldPos,
-						  const std::pair<double, double>& wantedPos_real,
-						  const std::pair<double, double>& size)
+bool    PhysicCollideLayer::checkCoordSizeCanPass(const std::pair<double, double> &oldPos,
+                                                  const std::pair<double, double> &wantedPos_real,
+                                                  const std::pair<double, double> &size)
 {
-  if (oldPos.first == wantedPos_real.first && oldPos.second == wantedPos_real.second)
-    return (true);
+    if (oldPos.first == wantedPos_real.first && oldPos.second == wantedPos_real.second)
+        return (true);
 
-  std::pair<double, double> start;
-  std::pair<double, double> end;
+    std::pair<double, double> start;
+    std::pair<double, double> end;
 
-  //push wantedPos forward so as to check if the body can fit in that place
-  std::pair<double, double> wantedPos = wantedPos_real;
-  if (wantedPos.first < oldPos.first)
-    wantedPos.first -= size.first;
-  else
-    wantedPos.first += size.first;
-  if (wantedPos.second < oldPos.second)
-    wantedPos.second -= size.second;
-  else
-    wantedPos.second += size.second;
+    std::pair<double, double> wantedPos = wantedPos_real;
 
-  start.second = oldPos.second + size.second / 2;
-  end.second = oldPos.second - size.second / 2;
-  if (oldPos.second < wantedPos.second)
-    {
-      if (oldPos.first < wantedPos.first)
-	{
-	  start.first = oldPos.first - size.first / 2;
-	  end.first = oldPos.first + size.first / 2;
-	}
-      else
-	{
-	  start.first = oldPos.first + size.first / 2;
-	  end.first = oldPos.first - size.first / 2;
-	}
-    }
-  else
-    {
-      if (oldPos.first < wantedPos.first)
-	{
-	  start.first = oldPos.first + size.first / 2;
-	  end.first = oldPos.first - size.first / 2;
-	}
-      else
-	{
-	  start.first = oldPos.first - size.first / 2;
-	  end.first = oldPos.first + size.first / 2;
-	}
-    }
+    //for size
+    int extra_points = (int)(sqrt(pow(size.first, 2) + pow(size.second, 2)) / HEIGHT_MAP_SCALE) + 1;
 
-  if (ABS(oldPos.first - wantedPos.first) > ABS(oldPos.second - wantedPos.second))
-    {
-      inc.first = (oldPos.first < wantedPos.first ? 1 : -1);
-      inc.second = ABS((oldPos.second - wantedPos.second) /
-		       (oldPos.first - wantedPos.first));
-      if (oldPos.second > wantedPos.second)
-	inc.second *= -1;
-      inc.first *= HEIGHT_MAP_SCALE;
-      inc.second *= HEIGHT_MAP_SCALE;
+    if (ABS(oldPos.first - wantedPos.first) > ABS(oldPos.second - wantedPos.second))
+        {
+            inc.first = (oldPos.first < wantedPos.first ? 1 : -1);
+            inc.second = ABS((oldPos.second - wantedPos.second) /
+                             (oldPos.first - wantedPos.first));
+            if (oldPos.second > wantedPos.second)
+                inc.second *= -1;
+            inc.first *= HEIGHT_MAP_SCALE;
+            inc.second *= HEIGHT_MAP_SCALE;
 
-      nb_points = ABS((oldPos.first - wantedPos.first) / inc.first);
-    }
-  else
-    {
-      inc.second = (oldPos.second < wantedPos.second ? 1 : -1);
-      inc.first = ABS((oldPos.first - wantedPos.first) /
-		      (oldPos.second - wantedPos.second));
-      if (oldPos.first > wantedPos.first)
-	inc.first *= -1;
-      inc.first *= HEIGHT_MAP_SCALE;
-      inc.second *= HEIGHT_MAP_SCALE;
+            nb_points = (int) (ABS((oldPos.first - wantedPos.first) / inc.first) + 1) + extra_points;
+        }
+    else
+        {
+            inc.second = (oldPos.second < wantedPos.second ? 1 : -1);
+            inc.first = ABS((oldPos.first - wantedPos.first) /
+                            (oldPos.second - wantedPos.second));
+            if (oldPos.first > wantedPos.first)
+                inc.first *= -1;
+            inc.first *= HEIGHT_MAP_SCALE;
+            inc.second *= HEIGHT_MAP_SCALE;
 
-      nb_points = (int)(ABS((oldPos.second - wantedPos.second) / inc.second) + 1);
-    }
+            nb_points = (int) (ABS((oldPos.second - wantedPos.second) / inc.second) + 1) + extra_points;
+        }
 
-  return (doOnLine(start, end, &PhysicCollideLayer::checkLine));
+    start.second = oldPos.second + size.second / 2;
+    end.second = oldPos.second - size.second / 2;
+    if (oldPos.second < wantedPos.second)
+        {
+            if (oldPos.first < wantedPos.first)
+                {
+                    start.first = oldPos.first - size.first / 2;
+                    end.first = oldPos.first + size.first / 2;
+                }
+            else
+                {
+                    start.first = oldPos.first + size.first / 2;
+                    end.first = oldPos.first - size.first / 2;
+                }
+        }
+    else
+        {
+            if (oldPos.first < wantedPos.first)
+                {
+                    start.first = oldPos.first + size.first / 2;
+                    end.first = oldPos.first - size.first / 2;
+                }
+            else
+                {
+                    start.first = oldPos.first - size.first / 2;
+                    end.first = oldPos.first + size.first / 2;
+                }
+        }
+    return (doOnLine(start, end, &PhysicCollideLayer::checkLine));
 }
 
 bool PhysicCollideLayer::doOnLine(std::pair<double, double> start,
-				  std::pair <double, double> end,
-				  bool (PhysicCollideLayer::*func)
-				  (std::pair<double, double>))
+                                  std::pair<double, double> end,
+                                  bool (PhysicCollideLayer::*func)
+                                          (std::pair<double, double>))
 {
-  double inc_y;
-  double inc_x;
+    double inc_y;
+    double inc_x;
 
-  if (ABS(start.first - end.first) > ABS(start.second - end.second))
-    {
-      inc_y = ABS((end.second - start.second) / (end.first - start.first));
-      if (start.second > end.second)
-	inc_y *= -1;
-      inc_x = (start.first < end.first ? 1 : -1);
-      inc_x *= HEIGHT_MAP_SCALE;
-      inc_y *= HEIGHT_MAP_SCALE;
+    if (ABS(start.first - end.first) > ABS(start.second - end.second))
+        {
+            inc_y = ABS((end.second - start.second) / (end.first - start.first));
+            if (start.second > end.second)
+                inc_y *= -1;
+            inc_x = (start.first < end.first ? 1 : -1);
+            inc_x *= HEIGHT_MAP_SCALE;
+            inc_y *= HEIGHT_MAP_SCALE;
 
-      if (start.first < end.first)
-        while (start.first <= end.first)
-          {
-            if (!(this->*func)(start))
-	      return (false);
-            start.second += inc_y;
-            start.first += inc_x;
-          }
-      else
-        while (start.first >= end.first)
-          {
-            if (!(this->*func)(start))
-	      return (false);
-            start.second += inc_y;
-            start.first += inc_x;
-          }
-    }
-  else
-    {
-      inc_y = (start.second < end.second ? 1 : -1);
-      inc_x = ABS((end.first - start.first) / (end.second - start.second));
-      if (start.first > end.first)
-	inc_x *= -1;
-      inc_x *= HEIGHT_MAP_SCALE;
-      inc_y *= HEIGHT_MAP_SCALE;
+            if (start.first < end.first)
+                while (start.first <= end.first)
+                    {
+                        if (!(this->*func)(start))
+                            return (false);
+                        start.second += inc_y;
+                        start.first += inc_x;
+                    }
+            else
+                while (start.first >= end.first)
+                    {
+                        if (!(this->*func)(start))
+                            return (false);
+                        start.second += inc_y;
+                        start.first += inc_x;
+                    }
+        }
+    else
+        {
+            inc_y = (start.second < end.second ? 1 : -1);
+            inc_x = ABS((end.first - start.first) / (end.second - start.second));
+            if (start.first > end.first)
+                inc_x *= -1;
+            inc_x *= HEIGHT_MAP_SCALE;
+            inc_y *= HEIGHT_MAP_SCALE;
 
-      if (start.second < end.second)
-        while (start.second <= end.second)
-          {
-            if (!(this->*func)(start))
-	      return (false);
-            start.second += inc_y;
-            start.first += inc_x;
-          }
-      else
-        while (start.second >= end.second)
-          {
-            if (!(this->*func)(start))
-	      return (false);
-            start.second += inc_y;
-            start.first += inc_x;
-          }
-    }
-  return (true);
+            if (start.second < end.second)
+                while (start.second <= end.second)
+                    {
+                        if (!(this->*func)(start))
+                            return (false);
+                        start.second += inc_y;
+                        start.first += inc_x;
+                    }
+            else
+                while (start.second >= end.second)
+                    {
+                        if (!(this->*func)(start))
+                            return (false);
+                        start.second += inc_y;
+                        start.first += inc_x;
+                    }
+        }
+    return (true);
 }
 
 bool PhysicCollideLayer::checkLine(std::pair<double, double> point)
 {
-  lastPoint = point;
-  std::pair<double, double> dest;
-  dest.first = point.first + inc.first * nb_points;
-  dest.second = point.second + inc.second * nb_points;
+    lastPoint = point;
+    std::pair<double, double> dest;
+    dest.first = point.first + inc.first * nb_points;
+    dest.second = point.second + inc.second * nb_points;
 
-  if (point.first < 0 || point.first >= sizeX ||
-      point.second < 0 || point.second >= sizeY ||
-      dest.first < 0 || dest.first >= sizeX ||
-      dest.second < 0 || dest.second >= sizeY)
-    return (false);
+    if (point.first < 0 || point.first >= sizeX ||
+        point.second < 0 || point.second >= sizeY ||
+        dest.first < 0 || dest.first >= sizeX ||
+        dest.second < 0 || dest.second >= sizeY)
+        return (false);
 
-  return (doOnLine(point, dest, &PhysicCollideLayer::checkPoint));
+    return (doOnLine(point, dest, &PhysicCollideLayer::checkPoint));
 }
 
 bool PhysicCollideLayer::checkPoint(std::pair<double, double> point)
 {
-  if (isWall(lastPoint, point))
-    return (false);
-  lastPoint = point;
-  return (true);
+    if (isWall(lastPoint, point))
+        return (false);
+    lastPoint = point;
+    return (true);
 }
 
 const unsigned char *PhysicCollideLayer::getLayer() const
 {
-  return (layer);
+    return (layer);
 }
